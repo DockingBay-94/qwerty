6aa6eb71fe4073270cfbd5cf07962a1a76d73dea
Xustom4994 <pbryson3.1415@gmail.com>
2022-07-21 16:36:30 -0700

    added big red square that teleports

--- qwerty3.js
+++ qwerty3.js
@@ -8,14 +8,25 @@ let message2 = document.querySelector(".message2")
 
 let button = document.querySelector("button")
 
-button.onclick = function(){
-    let thingWord = thingField.value
-    message1.textContent = "believe you said " + thingWord
+let red_square = document.querySelector(".red_square")
 
-    console.log(message1.textContent)
+function set_position_red_square(x, y){
+    red_square.style.left = x
+    red_square.style.top = y
+}
+
+button.onclick = function(){
+    // let thingWord = thingField.value
+    // message1.textContent = "believe you said " + thingWord
+    // console.log(message1.textContent)
+    set_position_red_square(10, 10)
 }
 otherbutton.onclick = function(){
     message2.textContent = "it works"
-
     console.log(message2.textContent)
 }
+
+
+// document.onkeydown() = function(){
+//     red_square.style.left
+// }
