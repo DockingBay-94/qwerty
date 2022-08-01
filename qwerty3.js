7973680657477f6bbe85c2b133379b658a88904f
Xustom4994 <pbryson3.1415@gmail.com>
2022-08-01 16:28:31 -0700

    made blue square

--- qwerty3.js
+++ qwerty3.js
@@ -12,14 +12,7 @@ let button = document.querySelector("button")
 
 let red_square = document.querySelector(".red_square")
 
-function set_position_red_square(x, y){
-    red_square.style.left = x
-    red_square.style.top = y
-}
-function move_position_red_square(x, y){
-    red_square.style.left = parseInt(red_square.style.left) + x
-    red_square.style.top = parseInt(red_square.style.top) + y
-}
+let blue_square = document.querySelector(".blue_square")
 
 button1.onclick = function(){
     let thingWord = input1.value
@@ -32,33 +25,73 @@ button2.onclick = function(){
     console.log(message2.textContent)
 }
 
+function set_position(x, y, element){
+    element.style.left = x
+    element.style.top = y
+}
+
+function move_position(x, y, element){
+    element.style.left = parseInt(element.style.left) + x
+    element.style.top = parseInt(element.style.top) + y
+}
+
+
 document.onkeydown = function(event){
     switch(event.key){
          case "t" :
-            set_position_red_square(100, 100)
+            set_position(100, 100, red_square)
          case "w" :
-            move_position_red_square(0, -10)
+            move_position(0, -10, red_square)
             break
          case "a" :
-            move_position_red_square(-10, 0)
+            move_position(-10, 0, red_square)
             break
          case "s" :
-            move_position_red_square(0, 10)
+            move_position(0, 10, red_square)
             break
          case "d" :
-            move_position_red_square(10, 0)
+            move_position(10, 0, red_square)
             break
          case "x" :
-            move_position_red_square(10, 10)
+            move_position(10, 10, red_square)
             break
          case "e" :
-            move_position_red_square(10, -10)
+            move_position(10, -10, red_square)
             break
          case "z" :
-            move_position_red_square(-10, 10)
+            move_position(-10, 10, red_square)
             break
          case "q" :
-            move_position_red_square(-10, -10)
+            move_position(-10, -10, red_square)
+            break
+
+
+
+         case "p" :
+            set_position(100, 100, blue_square)
+         case "u" :
+            move_position(0, -10, blue_square)
+            break
+         case "h" :
+            move_position(-10, 0, blue_square)
+            break
+         case "j" :
+            move_position(0, 10, blue_square)
+            break
+         case "k" :
+            move_position(10, 0, blue_square)
+            break
+         case "m" :
+            move_position(10, 10, blue_square)
+            break
+         case "i" :
+            move_position(10, -10, blue_square)
+            break
+         case "n" :
+            move_position(-10, 10, blue_square)
+            break
+         case "y" :
+            move_position(-10, -10, blue_square)
             break
     }
 }
