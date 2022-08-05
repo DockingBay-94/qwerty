eba9076a02cedea99aac6cf4063c1811ae187e34
Xustom4994 <pbryson3.1415@gmail.com>
2022-08-05 14:21:13 -0700

    buttons get removed and stuff

--- qwerty3.js
+++ qwerty3.js
@@ -23,6 +23,8 @@ let blue_square = document.querySelector(".blue_square")
 
 let green_square = document.querySelector(".green_square")
 
+let that_choose_thing = document.querySelector(".that_choose_thing")
+
 function set_position(x, y, element){
    element.style.left = x
    element.style.top = y
@@ -42,11 +44,11 @@ function send_position(element){
       EString: elementName
    }))
 }
-// function teleport_red(x, y){
-//    red_square.style.top = y
-//    red_square.style.left = x
-//    send_position(red_square)
-// }
+function teleport(x, y, element){
+   element.style.left = x
+   element.style.top = y
+   send_position(element)
+}
 
 function move_position(x, y, element){
    element.style.left = parseInt(element.style.left) + x
@@ -54,12 +56,19 @@ function move_position(x, y, element){
    send_position(element)
 }
 
-function choose_and_controll_square(used_button, unused_buttonA, unused_buttonB, color_message, square){
+function choose_and_controll_square(used_button, color_message, square){
    used_button.onclick = function(){
       message.textContent = color_message
       console.log(message.textContent)
-      unused_buttonA.textContent = "DO NOT PRESS"
-      unused_buttonB.textContent = "DO NOT PRESS"
+      button1.remove()
+      button2.remove()
+      button3.remove()
+      that_choose_thing.textContent =""
+      leftButton.textContent = "left"
+      upButton.textContent = "up"
+      downButton.textContent = "down"
+      rightButton.textContent = "right"
+
       leftButton.onclick = ()=>{
          move_position(-10, 0, square)
       }
@@ -75,7 +84,7 @@ function choose_and_controll_square(used_button, unused_buttonA, unused_buttonB,
       document.onkeydown = function(event){
          switch(event.key){
             case "t" :
-               set_position(100, 100, square)
+               teleport(100, 100, square)
                break
             case "w" :
                move_position(0, -10, square)
@@ -102,14 +111,8 @@ function choose_and_controll_square(used_button, unused_buttonA, unused_buttonB,
                move_position(-10, -10, square)
                break
             case "l" :
-               set_position(1, 1, square)
+               teleport(1, 1, square)
                break
-            // case "n" :
-            //    teleport_red(600, 600)
-            //    break
-            // case "m" :
-            //    teleport_red(50, 50)
-            //    break
          }
       }
    }
@@ -119,9 +122,9 @@ function choose_and_controll_square(used_button, unused_buttonA, unused_buttonB,
 
 holler.onLoad(()=>{
    holler.me((user)=>{ 
-      choose_and_controll_square(button1, button2, button3, "you are red", red_square)
-      choose_and_controll_square(button2, button1, button3, "you are blue", blue_square)
-      choose_and_controll_square(button3, button1, button2, "you are green", green_square)
+      choose_and_controll_square(button1, "you are red", red_square)
+      choose_and_controll_square(button2, "you are blue", blue_square)
+      choose_and_controll_square(button3, "you are green", green_square)
    })   
    holler.onClientEvent(event=>{ 
       console.log(event)
